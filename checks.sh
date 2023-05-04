22b37a24dc991ddd4cd3adaada19a1ced7a6c9c3
secureinfo42 <16359801+secureinfo42@users.noreply.github.com>
2023-05-04 15:31:42 +0200

    Update checks.sh

--- checks.sh
+++ checks.sh
@@ -32,53 +32,56 @@ echo > report.txt
 echo "=== Full RSA encryption test, buffer size : 128 bytes ===" >> report.txt
 echo >> report.txt
 
-# echo
-# echo "[+] Generating sample files..."
-# echo
+echo
+echo "[+] Generating sample from '/dev/urandom' files..."
+echo
 
-# gen_sample 1k   1   1024
-# gen_sample 10k  10  1024
-# gen_sample 100k 100 1024
-# gen_sample 1M   1   $((1024*1024))
+gen_sample 1k   1   1024
+gen_sample 10k  10  1024
+gen_sample 100k 100 1024
+gen_sample 1M   1   $((1024*1024))
+gen_sample 10M  10  $((1024*1024))
 
-# gen_keys 2048
+printf -- "\n%-11s | %-15s | %-20s | %-20s | %s" "RSA keysize" "Size (bytes)" "Encryption time (ms)" "Decryption time (ms)" "Checked (md5(decrypted) = md5(original)" >> report.txt
+printf -- "\n%-11s | %-15s | %-20s | %-20s | %s" "-----------" "------------" "--------------------" "--------------------" "-------" >> report.txt
+printf -- "\n%-11s | %-15s | %-20s | %-20s | %s" "RSA keysize" "Size (bytes)" "Encryption time (ms)" "Decryption time (ms)" "Checked (md5(decrypted) = md5(original)"
+printf -- "\n%-11s | %-15s | %-20s | %-20s | %s" "-----------" "------------" "--------------------" "--------------------" "-------"
 
+for s in 2048 4096 8192 ; do
 
-printf -- "\n%-15s | %-20s | %-20s | %s" "Size (bytes)" "Encryption time (ms)" "Decryption time (ms)" "Checked (md5(decrypted) = md5(original)" >> report.txt
-printf -- "\n%-15s | %-20s | %-20s | %s" "------------" "--------------------" "--------------------" "-------" >> report.txt
+	python3 rsacrypt.py -g -s $s
 
-printf -- "\n%-15s   | %-20s  | %-20s  | %s" "Size (bytes)" "Encryption time (ms)" "Decryption time (ms)" "Checked (md5(decrypted) = md5(original)"
-printf -- "\n%-15s   | %-20s  | %-20s  | %s" "------------" "--------------------" "--------------------" "-------"
+	for f in 1k 10k 100k 1M 10M ; do
 
-for f in 1k 10k 100k 1M ; do
+		rm -f $f.enc $f.dec
+		sz=$(stat -c "%s" "$f")
 
-	rm -f $f.enc $f.dec
-	sz=$(stat -c "%s" "$f")
+		printf "\n%-11s | " "$s bits"
+		printf "File : %-8s | " "$f"
+		printf "Encryption..."
 
-	printf "\nFile : %-10s | " "$f"
-	printf "Encryption..."
+		te1=$(date +"%s%N")
+		python3 rsacrypt.py -e -f $f -o $f.enc
+		te2=$(date +"%s%N")
+		te=$(echo "($te2-$te1)/1000000"|bc)
+		printf "\b\b\b : %-7s" "$te"
 
-	te1=$(date +"%s%N")
-	python3 rsacrypt -e  -f $f -o $f.enc
-	te2=$(date +"%s%N")
-	te=$(echo "($te2-$te1)/1000000"|bc)
-	printf "\b\b\b : %-8s" "$te"
+		printf " | Decryption..."
+		td1=$(date +"%s%N")
+		python3 rsacrypt.py -d  -f $f.enc -o $f.dec
+		td2=$(date +"%s%N")
+		td=$(echo "($td2-$td1)/1000000"|bc)
+		printf "\b\b\b : %-7s" "$td"
 
-	printf " | Decryption..."
-	td1=$(date +"%s%N")
-	python3 rsacrypt -d  -f $f.enc -o $f.dec
-	td2=$(date +"%s%N")
-	td=$(echo "($td2-$td1)/1000000"|bc)
-	printf "\b\b\b : %-8s" "$td"
+		c=$(md5sum $f $f.dec|cut -d' ' -f1|uniq|wc -l|grep -sq 1 && echo OK || echo KO)
+		printf " | Checking : $c"
 
-	c=$(md5sum $f $f.dec|cut -d' ' -f1|uniq|wc -l|grep -sq 1 && echo OK || echo KO)
-	printf " | Checking : $c"
+		printf -- "\n%-11s | %-15s | %-20s | %-20s | %s" "$s" "$sz" "$te" "$td" "$c" >> report.txt
 
-	printf -- "\n%-15s | %-20s | %-20s | %s" "$sz" "$te" "$td" "$c" >> report.txt
+	done
 
 done
 
-echo
 echo >> report.txt
 
 rm -f *.enc *.dec
